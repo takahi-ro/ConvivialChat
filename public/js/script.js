43dacede9819b9447616d1b65594d2843155ffe3
takahi-ro <57240543+takahi-ro@users.noreply.github.com>
2021-07-08 12:54:19 +0900

    replace the deployed one's code for the local codes.

--- public/js/script.js
+++ public/js/script.js
@@ -23,10 +23,10 @@ const Peer = window.Peer;
     UA: ${navigator.userAgent}
     SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
   `.trim();
- 
 
 
-  const getRoomModeByHash = () => (location.hash ='sfu');
+
+  const getRoomModeByHash = () => (location.hash = 'sfu');
 
   roomMode.textContent = getRoomModeByHash();
   window.addEventListener(
@@ -34,16 +34,16 @@ const Peer = window.Peer;
     () => (roomMode.textContent = getRoomModeByHash())
   );
 
-  
-  
-    var localStream = await navigator.mediaDevices
+
+
+  var localStream = await navigator.mediaDevices
     .getUserMedia({
-        audio: true,
-        video: true
+      audio: true,
+      video: true
     })
     .catch(console.error);
-  
-    console.log(localStream);
+
+  console.log(localStream);
 
   // Render local stream
   localVideo.muted = true;
@@ -51,26 +51,26 @@ const Peer = window.Peer;
   localVideo.playsInline = true;
   await localVideo.play().catch(console.error);
   //ストリームのオンオフ
-  const onoffSwitch = () =>{
+  const onoffSwitch = () => {
     var OnOff = document.getElementById("onoff");
     var onoff = OnOff.className;
     var OnOff2 = document.getElementById("onoff2");
     var onoff2 = OnOff2.className;
-  if(onoff == "toggle-btn active"){
-    localStream.getVideoTracks().forEach((track) => (track.enabled = true));
-  }else{
-    localStream.getVideoTracks().forEach((track) => (track.enabled = false));
+    if (onoff == "toggle-btn active") {
+      localStream.getVideoTracks().forEach((track) => (track.enabled = true));
+    } else {
+      localStream.getVideoTracks().forEach((track) => (track.enabled = false));
+    }
+    if (onoff2 == "toggle-btn active") {
+      localStream.getAudioTracks().forEach((track) => (track.enabled = true));
+    } else {
+      localStream.getAudioTracks().forEach((track) => (track.enabled = false));
+    }
+    // console.log(onoff);
+    // console.log(onoff2);
   }
-  if(onoff2 == "toggle-btn active"){
-    localStream.getAudioTracks().forEach((track) => (track.enabled = true));
-  }else{
-    localStream.getAudioTracks().forEach((track) => (track.enabled = false));
-  }
-  // console.log(onoff);
-  // console.log(onoff2);
-  }    
   setInterval(onoffSwitch, 1000);
-  
+
 
   // eslint-disable-next-line require-atomic-updates
   const peer = (window.peer = new Peer({
@@ -85,32 +85,35 @@ const Peer = window.Peer;
     if (!peer.open) {
       return;
     }
-    
-   //この下で相手に渡すデータを決めている
-   const room = peer.joinRoom(roomId.value, {
-    mode: getRoomModeByHash(),
-    stream: localStream,
-  }, {metadata: {
-    name:Yourname.value
-  }});
 
-   console.log(room);
+    //この下で相手に渡すデータを決めている
+    const room = peer.joinRoom(roomId.value, {
+      mode: getRoomModeByHash(),
+      stream: localStream,
+    }, {
+      metadata: {
+        name: Yourname.value
+      }
+    });
+
+    console.log(room);
 
     room.once('open', () => {
       messages.textContent += '=== You joined ===\n';
       console.log(getRoomModeByHash);
-      // console.log(room);
-    
+      console.log(room);
+
     });
 
 
 
     room.on('peerJoin', (peerId) => {
+
       messages.textContent += `=== ${peerId} joined ===\n`;
       // console.log(metadata);
     });
 
-    
+
 
     // Render remote stream for new peer join in the room この下が相手に送るデータを定めているはずだからここを直せば名前の件は解決するはず。
     room.on('stream', async stream => {
@@ -125,21 +128,21 @@ const Peer = window.Peer;
     });
 
 
-  
-     
+
+
 
     room.on('data', ({ data, src }) => {
       // Show a message sent to the room and who sent
-    if(data.match(/「/)){
-      var msg = new SeechSynthesisUtterance();
-      var text = data;
-      msg.volume = 1; //ボリューム
-      msg.rate = 1;  //レート
-      msg.pitch = 1; //ピッチ
-      msg.text = text;
-      msg.lang = 'ja-JP'; //言語
-      window.speechSynthesis.speak(msg);
-    }
+      if (data.match(/「/)) {
+        var msg = new SeechSynthesisUtterance();
+        var text = data;
+        msg.volume = 1; //ボリューム
+        msg.rate = 1;  //レート
+        msg.pitch = 1; //ピッチ
+        msg.text = text;
+        msg.lang = 'ja-JP'; //言語
+        window.speechSynthesis.speak(msg);
+      }
       messages.textContent += `${data}\n`;
       console.log(src);
 
@@ -148,7 +151,7 @@ const Peer = window.Peer;
         messages.scrollTop = messages.scrollHeight;
       };
       scrollToBottom();
-      
+
     });
 
     // for closing room members
@@ -174,106 +177,108 @@ const Peer = window.Peer;
       });
     });
 
-       
 
-    
-      SpeechToText();
-      sendTrigger.addEventListener('click', onClickSend);
-      sendTrigger2.addEventListener('click', onClickSend2);
-      leaveTrigger.addEventListener('click', () => room.close(), { once: true }); 
 
-      function onClickSend() {
-        // Send message to all of the peers in the room via websocket
-        if(localText.value == ''){
-          console.log("text value is null");}else{
-             //ワードクラウド（以下4行）
-            // targettext = myWords.push({
-            //   word: localText.value,size: Math.floor((Math.random()+0.1)*30)  
-            //   });
-            //   WordCloud();
-            let saytext = `「${localText.value}」`;
-            let senddata1 = `${Yourname.value}: ${saytext}`;
-            room.send(senddata1);
-            messages.textContent += `${senddata1}\n`;
-            localText.value = '';
-           
-          }
+
+    SpeechToText();
+    sendTrigger.addEventListener('click', onClickSend);
+    sendTrigger2.addEventListener('click', onClickSend2);
+    leaveTrigger.addEventListener('click', () => room.close(), { once: true });
+
+    function onClickSend() {
+      // Send message to all of the peers in the room via websocket
+      if (localText.value == '') {
+        console.log("text value is null");
+      } else {
+        //ワードクラウド（以下4行）
+        // targettext = myWords.push({
+        //   word: localText.value,size: Math.floor((Math.random()+0.1)*30)  
+        //   });
+        //   WordCloud();
+        let saytext = `「${localText.value.trim()}」`;
+        let senddata1 = `${Yourname.value}: ${saytext}`;
+        room.send(senddata1);
+        messages.textContent += `${senddata1}\n\n`;
+        localText.value = '';
+
       }
-      function onClickSend2() {
-        // Send message to all of the peers in the room via websocket
-        if(localText.value == ''){
-          console.log("text value is null");}else{
-            //ワードクラウド（以下4行）
-            // targettext = myWords.push({
-            //   word: localText.value,size: Math.floor((Math.random()+0.1)*30)  
-            //   });
-            //   WordCloud();
-            let senddata2 = `${Yourname.value}: ${localText.value}`;
-            room.send(senddata2);    
-            messages.textContent += `${senddata2}\n`;
-            localText.value = '';
-          
-          }
-         }
-      function SpeechToText(){
-        const startBtn = document.querySelector('#start-btn');
-        const stopBtn = document.querySelector('#stop-btn');
-        // const resultDiv = document.querySelector('#result-div');
-        SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
-        let recognition = new SpeechRecognition();
-        recognition.lang = 'ja-JP';
-        recognition.interimResults = true;
-        recognition.continuous = true;
+    }
+    function onClickSend2() {
+      // Send message to all of the peers in the room via websocket
+      if (localText.value == '') {
+        console.log("text value is null");
+      } else {
+        //ワードクラウド（以下4行）
+        // targettext = myWords.push({
+        //   word: localText.value,size: Math.floor((Math.random()+0.1)*30)  
+        //   });
+        //   WordCloud();
+        let senddata2 = `${Yourname.value}: ${localText.value.trim()}`;
+        room.send(senddata2);
+        messages.textContent += `${senddata2}\n\n`;
+        localText.value = '';
 
-        let finalTranscript = ''; // 確定した(黒の)認識結果
+      }
+    }
+    function SpeechToText() {
+      const startBtn = document.querySelector('#start-btn');
+      const stopBtn = document.querySelector('#stop-btn');
+      // const resultDiv = document.querySelector('#result-div');
+      SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+      let recognition = new SpeechRecognition();
+      recognition.lang = 'ja-JP';
+      recognition.interimResults = true;
+      recognition.continuous = true;
 
-        recognition.onresult = (event) => {
-          let interimTranscript = ''; // 暫定(灰色)の認識結果
-          for (let i = event.resultIndex; i < event.results.length; i++) {
-            let transcript = event.results[i][0].transcript;
-            if (event.results[i].isFinal) {
-              finalTranscript += transcript;
-              let speechtext = `『${event.results[event.results.length-1][0].transcript}』`;
-              let senddata3 =  `${Yourname.value}:${speechtext}`;
-              room.send(senddata3);
-              messages.textContent +=`${senddata3}\n`;
-              // let finalspeech = `(音声）${event.results[event.results.length-1][0].transcript}`;
-              // room.send(finalspeech);
-              // messages.textContent += `${Yourname.value}: ${finalspeech}\n`;
-              
-              //チャットを一番下までスクロールさせる
-              var scrollToBottom = () => {
-                messages.scrollTop = messages.scrollHeight;
-              };
-              scrollToBottom();
-           
+      let finalTranscript = ''; // 確定した(黒の)認識結果
+
+      recognition.onresult = (event) => {
+        let interimTranscript = ''; // 暫定(灰色)の認識結果
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          let transcript = event.results[i][0].transcript;
+          if (event.results[i].isFinal) {
+            finalTranscript += transcript;
+            let speechtext = `『${event.results[event.results.length - 1][0].transcript}』`;
+            let senddata3 = `${Yourname.value}:${speechtext}`;
+            room.send(senddata3);
+            messages.textContent += `${senddata3}\n\n`;
+            // let finalspeech = `(音声）${event.results[event.results.length-1][0].transcript}`;
+            // room.send(finalspeech);
+            // messages.textContent += `${Yourname.value}: ${finalspeech}\n`;
+
+            //チャットを一番下までスクロールさせる
+            var scrollToBottom = () => {
+              messages.scrollTop = messages.scrollHeight;
+            };
+            scrollToBottom();
+
             //ワードクラウド
-                  // targettext = myWords.push({
-                  // word: event.results[event.results.length-1][0].transcript,size: Math.floor((Math.random()+0.1)*30)  
-                  // });
-                  // WordCloud();
-                  
+            // targettext = myWords.push({
+            // word: event.results[event.results.length-1][0].transcript,size: Math.floor((Math.random()+0.1)*30)  
+            // });
+            // WordCloud();
 
-            } else {
-              interimTranscript = transcript;
-            }
+
+          } else {
+            interimTranscript = transcript;
           }
-          // resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
-          // console.log(event);
         }
-
-        
-
-        startBtn.onclick = () => {
-          recognition.start();
-        }
-        stopBtn.onclick = () => {
-          recognition.stop();
-        }//ここまでがSpeech to text
+        // resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+        // console.log(event);
       }
 
-      
-   
+
+
+      startBtn.onclick = () => {
+        recognition.start();
+      }
+      stopBtn.onclick = () => {
+        recognition.stop();
+      }//ここまでがSpeech to text
+    }
+
+
+
   });
 
   peer.on('error', console.error);
@@ -283,37 +288,37 @@ const Peer = window.Peer;
 )();
 
 
- // ここから下はオンオフボタンのコード
- $('.cb-value').click(function() {
+// ここから下はオンオフボタンのコード
+$('.cb-value').click(function () {
   var mainParent = $(this).parent('.toggle-btn');
-  if($(mainParent).find('input.cb-value').is(':checked')) {
+  if ($(mainParent).find('input.cb-value').is(':checked')) {
     $(mainParent).addClass('active');
-   
+
   } else {
     $(mainParent).removeClass('active');
-    
+
   }
 })
 
 
 
 //JOINボタンをクリックする
-const ClickJoinButton = () =>{
+const ClickJoinButton = () => {
   const joinTrigger = document.getElementById('js-join-trigger');
   joinTrigger.click();
   console.log("You can join the room!");
 };
 
-setTimeout(ClickJoinButton,3000)
+setTimeout(ClickJoinButton, 3000)
 
 
 //以下はテキストtoスピーチ
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function () {
   var speech = new Speech();
   speech.init();
 }, null);
 
-function Speech(){
+function Speech() {
   this.textValue = null;
   this.langValue = null;
   this.volumeValue = null;
@@ -326,7 +331,7 @@ function Speech(){
   this.unsupported = 'Speech Synthesis is unsupported!';
 }
 
-Speech.prototype.init = function(){
+Speech.prototype.init = function () {
   var self = this;
   if ('speechSynthesis' in window) {
     self.message.textContent = self.support;
@@ -337,7 +342,7 @@ Speech.prototype.init = function(){
   }
   self.event();
 };
-Speech.prototype.getTextValue = function(){
+Speech.prototype.getTextValue = function () {
   return this.textValue = this.text.value;
 };
 //   Speech.prototype.getLangValue = function(){
@@ -352,7 +357,7 @@ Speech.prototype.getTextValue = function(){
 //   Speech.prototype.getPitchValue = function(){
 //     return this.pitchValue = document.getElementById('pitch').value;
 //   };
-Speech.prototype.setSpeech = function(){
+Speech.prototype.setSpeech = function () {
   var msg = new SpeechSynthesisUtterance();
   var text = this.getTextValue();
   // var lang = this.getLangValue();
@@ -366,9 +371,9 @@ Speech.prototype.setSpeech = function(){
   msg.lang = 'ja-JP';
   window.speechSynthesis.speak(msg);
 };
-Speech.prototype.event = function(){
+Speech.prototype.event = function () {
   var self = this;
-  self.btn.addEventListener('click', function(){ self.setSpeech(); }, null);  
+  self.btn.addEventListener('click', function () { self.setSpeech(); }, null);
 };
 
 
@@ -376,11 +381,11 @@ Speech.prototype.event = function(){
 
 //Yahoo テキスト解析
 // const  textYahooAPI =() => {
- 
-  
+
+
 //     const ns = XmlService.getNamespace("urn:yahoo:jp:jlp");
 //     const result = parseText("君は君らしく生きていく自由があるんだ");
-    
+
 //     const doc = XmlService.parse(result.getContentText());
 //     const root = doc.getRootElement();
 //     const words = root.getChild("ma_result", ns).getChild("word_list", ns).getChildren("word", ns);
@@ -395,12 +400,12 @@ Speech.prototype.event = function(){
 //   const yahooUrl = "https://jlp.yahooapis.jp/MAService/V1/parse";
 //   const appid = "dj00aiZpPWlXVW9WcWs3S1FyZyZzPWNvbnN1bWVyc2VjcmV0Jng9ZGM-";
 //   const url = yahooUrl + "?appid=" + appid;
-  
+
 //   const payload = {
 //     "sentence": text,
 //     "results": "ma,uniq"
 //   };
-  
+
 //   const params = {
 //     "method": "post",
 //     "muteHttpExceptions": true,
@@ -410,75 +415,75 @@ Speech.prototype.event = function(){
 
 
 // ここからはワードクラウド
-var myWords =[
-  //   // {"word":"イノシシ","size":10}
-  //   // {"word":"おにやんま","size":6},
-  //   // {"word":"ゆるっと","size":8},
-  //   // {"word":"映画","size":6},
-  //   // {"word":"ヘルシンキ","size":20},
-  //   // {"word":"メタリカ","size":15},
-  //   // {"word":"お面","size":10},
-  //   // {"word":"おいしい","size":20},
-  ];
-var w = 1320,
-    h = 1078,
-    sizeScale = d3.scaleLinear().domain([0, myWords.length]).range([10, 100]),
-    layout = d3.layout.cloud(),
-  // svgオブジェクトの追加
-    svg = d3.select("#cloud").append("svg")
-      .attr("class", "ui fluid image") 
-      .attr("viewBox", "0 0 " + w + " " + h ) 
-        .attr("width", "100%" )
-        .attr("height","100%")
-        .append("g");
-      
-   
+// var myWords =[
+//   //   // {"word":"イノシシ","size":10}
+//   //   // {"word":"おにやんま","size":6},
+//   //   // {"word":"ゆるっと","size":8},
+//   //   // {"word":"映画","size":6},
+//   //   // {"word":"ヘルシンキ","size":20},
+//   //   // {"word":"メタリカ","size":15},
+//   //   // {"word":"お面","size":10},
+//   //   // {"word":"おいしい","size":20},
+//   ];
+// var w = 1320,
+//     h = 1078,
+//     sizeScale = d3.scaleLinear().domain([0, myWords.length]).range([10, 100]),
+//     layout = d3.layout.cloud(),
+//   // svgオブジェクトの追加
+//     svg = d3.select("#cloud").append("svg")
+//       .attr("class", "ui fluid image") 
+//       .attr("viewBox", "0 0 " + w + " " + h ) 
+//         .attr("width", "100%" )
+//         .attr("height","100%")
+//         .append("g");
 
-  
-  // インスタンスの作成
 
-    var WordCloud = (wordcloud) => {
-      
-      layout
-      .size([w, h])
-      .words(myWords.map( function (d) { return { text: d.word, size: sizeScale(d.size)}; }))
-      .padding(10)        //単語の距離
-      .rotate(function () { return (~~(Math.random() * 6) - 3)*30; })
-      .fontSize(function (d) { return d.size; })      // フォントサイズ
-      .on("end", draw);
-      var wordcloud = () =>{
-      layout.start();
-      console.log(myWords);
-      };
-      setInterval(wordcloud,3000);
-      
-    // 'ayoutの出力を受け取り単語を描画
-    function draw(words) {
-          //  var socket;
-          //  socket = io.connect("http://localhost:3000");
-           svg
-          // style using semantic ui
-          .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
-          
-          .selectAll("text")
-          .data(words)
-          .enter().append("text")
-          .style("font-size", function (d) { return d.size+ "px"; })
-          .style("font-family", "Impact")
-          .attr("fill", function(d, i) { return d3.schemeCategory10[i % 10]; } )
-          .attr("text-anchor", "middle")
-          .attr("transform",function(d) {
-                      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-          })
-          //  .attr("transform",function(d) {
-          //             return "translate(" + [d.x, d.y] + ")";
-          // })
-          .text(function (d) { return d.text; });
-         
-    }
-    };
 
-  
+
+//   // インスタンスの作成
+
+//     var WordCloud = (wordcloud) => {
+
+//       layout
+//       .size([w, h])
+//       .words(myWords.map( function (d) { return { text: d.word, size: sizeScale(d.size)}; }))
+//       .padding(10)        //単語の距離
+//       .rotate(function () { return (~~(Math.random() * 6) - 3)*30; })
+//       .fontSize(function (d) { return d.size; })      // フォントサイズ
+//       .on("end", draw);
+//       var wordcloud = () =>{
+//       layout.start();
+//       console.log(myWords);
+//       };
+//       setInterval(wordcloud,3000);
+
+//     // 'ayoutの出力を受け取り単語を描画
+//     function draw(words) {
+//           //  var socket;
+//           //  socket = io.connect("http://localhost:3000");
+//            svg
+//           // style using semantic ui
+//           .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+
+//           .selectAll("text")
+//           .data(words)
+//           .enter().append("text")
+//           .style("font-size", function (d) { return d.size+ "px"; })
+//           .style("font-family", "Impact")
+//           .attr("fill", function(d, i) { return d3.schemeCategory10[i % 10]; } )
+//           .attr("text-anchor", "middle")
+//           .attr("transform",function(d) {
+//                       return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+//           })
+//           //  .attr("transform",function(d) {
+//           //             return "translate(" + [d.x, d.y] + ")";
+//           // })
+//           .text(function (d) { return d.text; });
+
+//     }
+//     };
+
+
 
 
 
