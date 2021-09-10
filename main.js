a07659cf4509ff4f00f9d494a0e1da8cdb2203e4
Takahiro Fujii <57240543+takahi-ro@users.noreply.github.com>
2021-09-10 17:38:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,11 +15,6 @@ const express = require("express"),
     process.env.MONGODB_URI || "mongodb://localhost:27017/skyway_db",
     {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true}
   );
-  // ヘロクデプロイ前
-  // mongoose.connect(
-  //   "mongodb://localhost:27017/skyway_db",
-  //   {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true}
-  // );
   
   const db = mongoose.connection;
 
@@ -30,10 +25,6 @@ const express = require("express"),
 app.set("view engine", "ejs");
 
 app.set("port", process.env.PORT || 3000);
-//ヘロクデプロイで追加した
-// const server = app.listen(app.get("port"), () => {
-// console.log(`Server running at http://localhost: ${app.get("port")}`)
-// });
 
 app.use(
   express.urlencoded({
@@ -47,34 +38,14 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
-// app.post("/home", homeController.Joinmeeting);
 app.post("/home",homeController.saveParticipant);
 
 app.get("/participants",homeController.getAllparticipants, (req, res, next) => {
   res.render("participants", { participants: req.data });
 });
 
-
-// app.get("/courses", homeController.showCourses);
-// app.get("/contact", homeController.showSignUp);
-// app.post("/contact", homeController.postedSignUpForm);
-
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
-
-//モデルにデータを入れる
-// var participant1 = new Participant({
-//   roomname:5555,
-//   yourname:"yuya"
-// });
-
-// participant1.save((error, savedDocument) => {
-// if(error) console.log(error);
-// console.log(savedDocument);
-// });
-
-
-
 
 app.listen(app.get("port"), () => {
     console.log(`Server running at http://localhost:${app.get("port")}`);
