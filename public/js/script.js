f1ee451b52e1e27e64f8d8d88fee74e69188804a
takahi-ro <57240543+takahi-ro@users.noreply.github.com>
2022-05-30 16:19:50 +0900

    Update script.js
    なぜかタイムインターバルしていた。しなくても大丈夫。

--- public/js/script.js
+++ public/js/script.js
@@ -56,7 +56,7 @@ let synth = window.speechSynthesis;
     })
     .catch(console.error);
 
-  //ストリームのオンオフ
+  //マイクのオンオフ
   const onoffSwitch = () => {
     let OnOff2 = document.getElementById("onoff2");
     let onoff2 = OnOff2.className;
@@ -66,7 +66,8 @@ let synth = window.speechSynthesis;
       localStream.getAudioTracks().forEach((track) => (track.enabled = false));
     }
   }
-  setInterval(onoffSwitch, 1000);
+
+  onoffSwitch();
 
 
   // eslint-disable-next-line require-atomic-updates
